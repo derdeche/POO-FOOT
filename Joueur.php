f07709823bd7eeaf7ea7af348108d3d51841a254
derdeche <foued.derdeche@gmail.com>
2022-12-19 16:33:48 +0100

    rajour construct function

--- Joueur.php
+++ Joueur.php
@@ -7,5 +7,15 @@ class Joueur
     private string $_DateNaissance;
     private Pays $_Pays;
     private array $_Carriere;
+
+    public function __construct(string $Nom, string $Prenom, string $DateNaissance, Pays $Pays)
+    {
+        $this->_Nom = $Nom;
+        $this->_Prenom = $Prenom;
+        $this->_DateNaissance = $DateNaissance;
+        $this->_Pays = $Pays;
+        $this->_Pays->ADDJoueur($this);
+        $this->_Carriere = [];
+    }
 }
 
